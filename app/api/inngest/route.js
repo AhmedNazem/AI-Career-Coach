67c856a4fd0fd0b8d38dacf2e7b4608fcb56a2cf
AhmedNazem <aahhaammdd55@gmail.com>
2025-02-15 04:06:40 +0300

    building up the Corn for weekly insights "Inngest function"

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,9 +1,11 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
+import { generateIndustryInsights } from "@/lib/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     // insert inngest  functions from functions.js
+    generateIndustryInsights,
   ],
 });
