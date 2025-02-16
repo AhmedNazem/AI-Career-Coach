40239e00b3cc05f69ced9e23bb1c3cc2972c14a7
AhmedNazem <aahhaammdd55@gmail.com>
2025-02-16 12:13:20 +0300

    building the interview performance stats page

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -1,7 +1,23 @@
+import { getAssessments } from "@/actions/interview";
 import React from "react";
+import StatsCards from "./_components/StatsCards";
+import PerformanceChart from "./_components/PerformanceChart";
+import QuizList from "./_components/QuizList";
 
-const Interview = () => {
-  return <div>Interview</div>;
+const Interview = async () => {
+  const assessments = await getAssessments();
+  return (
+    <div>
+      <h1 className="text-6xl font-bold gradient-title mb-5">
+        Interview Preparation
+      </h1>
+      <div className="space-y-6">
+        <StatsCards assessments={assessments} />
+        <PerformanceChart assessments={assessments} />
+        <QuizList assessments={assessments} />
+      </div>
+    </div>
+  );
 };
 
 export default Interview;
